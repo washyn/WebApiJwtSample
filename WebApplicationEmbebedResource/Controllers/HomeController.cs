5073002779f7fcd927e00871263c88fdcbfc4a39
Washington Acero M <shin321654987@gmail.com>
2023-09-01 12:00:41 -0700

    ade sample web app embebed resource

---
+++ WebApplicationEmbebedResource/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using WebApplicationEmbebedResource.Models;
+
+namespace WebApplicationEmbebedResource.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var manifestEmbeddedProvider = new ManifestEmbeddedFileProvider(typeof(Program).Assembly);
+        var file = manifestEmbeddedProvider.GetFileInfo("Views/bootstrap.css");
+        if (file.Exists)
+        {
+            var content = file.ReadAsString();
+            ViewData["style"] = content;
+        }
+        return View();
+    }
+
+    
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ WebApplicationEmbebedResource/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationEmbebedResource.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
