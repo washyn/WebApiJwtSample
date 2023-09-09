ba34576b19056a9d8d60d7b2e0f33be3c3bd8f88
Washington Acero M <shin321654987@gmail.com>
2023-09-08 18:08:51 -0700

    aded proyect

---
+++ ConsoleAppSampleOdbc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Data.Odbc;
+
+namespace ConsoleAppSampleOdbc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OdbcConnection DbConnection = new OdbcConnection("DSN=dbf_reniec");
+            DbConnection.Open();
+            // Your code here
+            Console.WriteLine("conected");
+            Console.ReadLine();
+            DbConnection.Close();
+        }
+    }
+}
