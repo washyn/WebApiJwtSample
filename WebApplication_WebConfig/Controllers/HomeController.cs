42760d7bfd6997cbcbfe83909ec3ed1245c15d46
Washington Acero M. <shin321654987@gmail.com>
2024-11-22 08:50:11 -0800

    aded proyect for show excample of environment conf

---
+++ WebApplication_WebConfig/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication_WebConfig.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var conexion = ConfigurationManager.ConnectionStrings["MyConnectionString"];
+            ViewData["con"] = conexion.ConnectionString;
+            ViewData["Example"] = ConfigurationManager.AppSettings["Example"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
