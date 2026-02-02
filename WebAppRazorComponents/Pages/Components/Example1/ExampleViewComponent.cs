3e3e9a778efa4e69f180f8315e4104f52f3b094e
Washington Acero M <shin321654987@gmail.com>
2026-02-01 18:25:42 -0800

    aded example of components

---
+++ WebAppRazorComponents/Pages/Components/Example1/ExampleViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppRazorComponents.Pages.Components.Example1;
+
+public class Example1ViewComponent : ViewComponent
+{
+    public virtual IViewComponentResult Invoke()
+    {
+        // TODO: add model
+        // Add  contributor system inheritable, same as abp configuration
+        return View("~/Pages/Components/Example1/Default.cshtml");
+        // TODO: replicar el sistema de permisos de abp, la contribucion
+    }
+}
---
+++ WebAppRazorComponents/Pages/Components/Example2/ExampleViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppRazorComponents.Pages.Components.Example2;
+
+public class Example2ViewComponent : ViewComponent
+{
+    public virtual IViewComponentResult Invoke()
+    {
+        return View("~/Pages/Components/Example2/Default.cshtml");
+    }
+}
