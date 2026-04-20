66b6070365ac01a913944b8e8ee23f4fdbf36280
Washington Acero M <shin321654987@gmail.com>
2026-04-19 19:02:19 -0700

    aded comments

--- Aws/AwsSnsReceiver/Program.cs
+++ Aws/AwsSnsReceiver/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
 
@@ -46,14 +48,45 @@ namespace AwsSnsReceiver
                 .UseSerilog()
                 .ConfigureServices((hostContext, services) =>
                 {
-                    // services.AddHostedService<AppHostedService>();
-                    // services.Configure<AwsSnsConfig>(a =>
-                    // {
-                    //     a.TopicArn = "arn:aws:sns:us-east-1:000000000000:mi-topic";
-                    //     a.AccessKeyId = "test";
-                    //     a.SecretAccessKey = "test";
-                    //     a.Region = "us-east-1";
-                    // });
+                    services.AddHostedService<AppHostedService>();
+                    services.Configure<AwsSnsConfig>(a =>
+                    {
+                        a.TopicArn = "arn:aws:sns:us-east-1:000000000000:mi-topic";
+                        a.AccessKeyId = "test";
+                        a.SecretAccessKey = "test";
+                        a.Region = "us-east-1";
+                    });
                 });
+    }
+
+    public class AwsSnsConfig
+    {
+        public string TopicArn { get; set; }
+        public string Region { get; set; }
+        public string AccessKeyId { get; set; }
+        public string SecretAccessKey { get; set; }
+    }
+
+    public class AppHostedService : IHostedService
+    {
+        private readonly ILogger<AppHostedService> _logger;
+        private readonly AwsSnsConfig _options;
+
+        public AppHostedService(ILogger<AppHostedService> logger, IOptions<AwsSnsConfig> options)
+        {
+            _logger = logger;
+            _options = options.Value;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Starting AppHostedService...");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Stopping AppHostedService...");
+            return Task.CompletedTask;
+        }
     }
 }
