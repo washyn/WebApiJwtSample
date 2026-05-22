883d6774d70737357f96c908f205fd47e9df222c
Washington Acero M <shin321654987@gmail.com>
2026-05-21 18:21:08 -0700

    ade config reader in lambda

--- AWSLambdaExample/AWSLambdaEmpty/Function.cs
+++ AWSLambdaExample/AWSLambdaEmpty/Function.cs
@@ -1,5 +1,7 @@
 using Amazon.Lambda.Core;
 
+using Microsoft.Extensions.Configuration;
+
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -7,7 +9,17 @@ namespace AWSLambdaEmpty;
 
 public class Function
 {
-    
+    public readonly IConfiguration configuration;
+
+    public Function()
+    {
+        // or can be build console host... same as sync product lambda
+        configuration = new ConfigurationBuilder()
+            .SetBasePath(Environment.CurrentDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+    }
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
