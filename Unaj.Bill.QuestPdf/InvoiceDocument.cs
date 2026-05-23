1b8f1ec79e9c5a859d5cffec0e71aeceb0484346
Washington Acero M <shin321654987@gmail.com>
2026-05-23 15:45:35 -0700

    removed unused code

--- Unaj.Bill.QuestPdf/InvoiceDocument.cs
+++ Unaj.Bill.QuestPdf/InvoiceDocument.cs
@@ -46,7 +46,6 @@ namespace QuestPDF.Invoice
 
                     if (!string.IsNullOrEmpty(_model.Seller?.LogoPath))
                     {
-                        // row.RelativeItem().Image(_model.Seller.LogoPath);
                         row.RelativeItem().Column(a => a.Item()
                             .Width(40).Image("opera.png"));
                     }
@@ -58,11 +57,9 @@ namespace QuestPDF.Invoice
 
                     row.RelativeItem(5).Column(col =>
                     {
-                        // col.Spacing();
                         col.Item().AlignCenter().Text(_model.Seller?.Name ?? string.Empty).Medium().FontSize(12);
                         col.Item().AlignCenter().Text(_model.Seller?.Subtext1 ?? string.Empty);
                         col.Item().AlignCenter().Text(_model.Seller?.Subtext2 ?? string.Empty);
-                        // col.Item().AlignCenter().Text(_model.Seller?.Subtext3 ?? string.Empty);
                     });
 
                     row.RelativeItem().Text(string.Empty);
@@ -86,70 +83,53 @@ namespace QuestPDF.Invoice
                 column.Item().PaddingVertical(2).LineHorizontal(1).LineColor(Colors.White);
 
                 // Información del Cliente
-                column.Item().Row(row =>
+                column.Item().Column(col =>
                 {
-                    row.Spacing(10);
-                    row.RelativeItem(3).Column(col =>
+                    col.Item().Text(text =>
                     {
-                        col.Item().Text(text =>
-                        {
-                            text.Span("Nombre y apellido: ").SemiBold();
-                            text.Span(_model.Customer?.Name ?? string.Empty);
-                        });
-                        // col.Item().Text(text =>
-                        // {
-                        //     text.Span("Nº de documento: ").SemiBold();
-                        //     text.Span(_model.Customer?.DocumentNumber ?? string.Empty);
-                        // });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("Medio de pago: ").SemiBold();
-                            text.Span("Billetera Digital Bipay");
-                        });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("Fecha de operación: ").SemiBold();
-                            text.Span(_model.Details?.IssueDate.ToString("D"));
-                        });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("Celular(Bipay): ").SemiBold();
-                            text.Span("997 *** 563");
-                        });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("ID transacción Bipay: ").SemiBold();
-                            text.Span("235614");
-                        });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("Nº de comprobante: ").SemiBold();
-                            text.Span("UNAJ-2026-0A54AEC8");
-                        });
-                        col.Item().Text(text =>
-                        {
-                            text.Span("ID solicitud UNAJ: ").SemiBold();
-                            text.Span("0a54aec8-d872-8e8e-48f4-3a212ed15e23");
-                        });
-
-                        // Tipo de documento DNI
+                        text.Span("Nombre y apellido: ").SemiBold();
+                        text.Span(_model.Customer?.Name ?? string.Empty);
                     });
-                    row.RelativeItem(2).Column(col =>
+
+                    col.Item().Text(text =>
                     {
-                        // col.Item().Text(text =>
-                        // {
-                        //     text.Span("Fecha de operación: ").SemiBold();
-                        //     text.Span(_model.Details?.IssueDate.ToString("d", cultureInfo) ?? string.Empty);
-                        // });
+                        text.Span("Medio de pago: ").SemiBold();
+                        text.Span("Billetera Digital Bipay");
+                    });
+                    col.Item().Text(text =>
+                    {
+                        text.Span("Fecha de operación: ").SemiBold();
+                        text.Span(_model.Details?.IssueDate.ToString("D"));
+                    });
+                    col.Item().Text(text =>
+                    {
+                        text.Span("Celular(Bipay): ").SemiBold();
+                        text.Span("997 *** 563");
+                    });
+                    col.Item().Text(text =>
+                    {
+                        text.Span("ID transacción Bipay: ").SemiBold();
+                        text.Span("235614");
+                    });
+                    col.Item().Text(text =>
+                    {
+                        text.Span("Nº de comprobante: ").SemiBold();
+                        text.Span("UNAJ-2026-0A54AEC8");
+                    });
+                    col.Item().Text(text =>
+                    {
+                        text.Span("ID solicitud UNAJ: ").SemiBold();
+                        text.Span("0a54aec8-d872-8e8e-48f4-3a212ed15e23");
                     });
                 });
 
+
                 column.Item().PaddingVertical(2).LineHorizontal(1).LineColor(Colors.White);
 
-                // Tabla de Items
+
                 column.Item().Element(ComposeTable);
 
-                // Sección de Totales
+
                 column.Item().PaddingBottom(5).Row(row =>
                 {
                     row.Spacing(10);
@@ -166,49 +146,22 @@ namespace QuestPDF.Invoice
                 });
 
 
-                // Firma / Pie de Página Informativo (QR e información de pago)
-                // column.Item().Column(col =>
-                // {
-                //     if (!string.IsNullOrEmpty(_model.HashImagePath))
-                //     {
-                //         col.Item().Width(50).Image(_model.HashImagePath);
-                //     }
-                //     col.Item().Text(a =>
-                //     {
-                //         a.Span("Codigo hash: ").SemiBold();
-                //         a.Span(_model.HashCode ?? string.Empty);
-                //     });
-                //     col.Item().Text(a =>
-                //     {
-                //         a.Span("Condicion pago: ").SemiBold();
-                //         a.Span(_model.PaymentMethod ?? string.Empty);
-                //     });
-                // });
-
                 column.Item().Background(Colors.Grey.Lighten3).Padding(8).Row(row =>
                 {
-                    // row.Spacing(50);
-
                     row.RelativeItem(10).Column(a =>
                     {
-                        // a.Spacing(5);
                         a.Item().Text(text =>
                         {
-                            // text.Span("Mensaje: ").SemiBold();
                             text.Span("AUTENTICIDAD DEL DOCUMENTO").FontSize(8);
                         });
 
                         a.Item().Text(text =>
                         {
-                            // text.Span("Mensaje de advertencia: ").SemiBold();
                             text.Span(
                                     "Este comprobante fue generado automácamente tras confirmar el pago en Bipay.Para verificar su autencidad, ulice el código o el código QR.")
                                 .FontSize(8);
                         });
-//                         Código de verificación
-// EA828F8E361C
-// URL de verificaciónhttps://pagos.unaj.edu.pe/verificar?r=0a54aec8-
-// d872-8e8e-48f4-3a212ed15e23&c=EA828F8E361C
+
                         a.Item().Text(text =>
                         {
                             text.Span("Código de verificación: ").FontSize(8);
@@ -236,17 +189,15 @@ namespace QuestPDF.Invoice
                     a.Spacing(5);
                     a.Item().Text(text =>
                     {
-                        // text.Span("Mensaje: ").SemiBold();
                         text.Span(_model.Message ?? string.Empty).Italic().FontSize(7);
                     });
                     a.Item().Text(text =>
                     {
-                        // text.Span("Mensaje de advertencia: ").SemiBold();
                         text.Span(_model.MessageWarning ?? string.Empty).FontSize(7);
                     });
                 });
 
-                // add Emido electrónicamente por UNAJ — UNAJ-2026-0A54AEC8
+
                 column.Item().PaddingVertical(5).Column(col =>
                 {
                     col.Item().AlignCenter().Text(text =>
@@ -268,8 +219,7 @@ namespace QuestPDF.Invoice
                 {
                     columns.ConstantColumn(35);
                     columns.RelativeColumn(3);
-                    // columns.RelativeColumn();
-                    // columns.RelativeColumn();
+
                     columns.RelativeColumn();
                 });
 
@@ -277,8 +227,7 @@ namespace QuestPDF.Invoice
                 {
                     header.Cell().Text("Codigo").Style(headerStyle);
                     header.Cell().Text("Concepto").Style(headerStyle);
-                    // header.Cell().AlignRight().Text("P.U.").Style(headerStyle);
-                    // header.Cell().AlignRight().Text("C.").Style(headerStyle);
+
                     header.Cell().AlignRight().Text("Importe").Style(headerStyle);
 
                     header.Cell().ColumnSpan(3).PaddingTop(2).Border(0.5F).BorderColor(Colors.Grey.Darken1);
@@ -290,8 +239,6 @@ namespace QuestPDF.Invoice
                     {
                         table.Cell().Element(CellStyle).AlignCenter().Text($"{item.Index}");
                         table.Cell().Element(CellStyle).Text(item.Description ?? string.Empty);
-                        // table.Cell().Element(CellStyle).AlignRight().Text($"{item.UnitPrice:C}");
-                        // table.Cell().Element(CellStyle).AlignRight().Text($"{item.Quantity}");
                         table.Cell().Element(CellStyle).AlignRight().Text($"{item.Total:C}");
 
                         static IContainer CellStyle(IContainer container) => container.BorderBottom(0.5F)
