6a86dfbc428fb559a050ec91de02bf6d57c46750
Washington Acero M <shin321654987@gmail.com>
2026-06-08 18:38:57 -0700

    aded abp core for front work

--- Angular/frontapp/src/app/app.config.ts
+++ Angular/frontapp/src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { AuthService, provideAbpCore, withOptions } from '@abp/ng.core';
+import { environment } from '../environments/environment';
+import { registerLocaleForEsBuild } from '@abp/ng.core/locale';
+import { CustomAuthService } from './custom-auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAbpCore(
+      withOptions({
+        environment,
+        registerLocaleFn: registerLocaleForEsBuild(),
+      }),
+    ),
+    {
+      provide: AuthService,
+      useClass: CustomAuthService,
+    },
   ]
 };
