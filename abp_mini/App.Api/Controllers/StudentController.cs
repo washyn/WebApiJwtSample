57a8f790e389862003f1d26220a39ba8525b87ef
Washington Acero M <shin321654987@gmail.com>
2026-06-10 20:13:13 -0700

    aded example crud

---
+++ abp_mini/App.Api/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+using App.Api.Dtos;
+using App.Api.Services;
+
+using Library.Application.Dtos;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Api.Controllers;
+
+[Route("api/student")]
+[ApiController]
+public class StudentController : ControllerBase
+{
+    private readonly StudentAppService _studentAppService;
+
+    public StudentController(StudentAppService studentAppService)
+    {
+        _studentAppService = studentAppService;
+    }
+
+    [HttpGet]
+    public async Task<PagedResultDto<BookDto>> GetStudents(PagedAndSortedResultRequestDto input)
+    {
+        return await _studentAppService.GetListAsync(input);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BookDto>> GetStudent(Guid id)
+    {
+        return await _studentAppService.GetAsync(id);
+    }
+
+    [HttpPost]
+    public async Task<BookDto> CreateStudent(BookDto studentDto)
+    {
+        return await _studentAppService.CreateAsync(studentDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<BookDto>> UpdateStudent(Guid id, BookDto studentDto)
+    {
+        return await _studentAppService.UpdateAsync(id, studentDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task DeleteStudent(Guid id)
+    {
+        await _studentAppService.DeleteAsync(id);
+    }
+}
