95018e1b122f70036c072b96d721cf9352251e5e
Washington Acero M <shin321654987@gmail.com>
2026-06-14 19:39:08 -0700

    aded validation completed

--- Angular/frontapp/src/app/app.config.ts
+++ Angular/frontapp/src/app/app.config.ts
@@ -1,10 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthService, provideAbpCore, withOptions } from '@abp/ng.core';
 import { environment } from '../environments/environment';
 import { registerLocaleForEsBuild } from '@abp/ng.core/locale';
 import { CustomAuthService } from './custom-auth.service';
+import {
+  BLUEPRINTS,
+  defaultMapErrorsFn,
+  VALIDATION_BLUEPRINTS,
+  VALIDATION_ERROR_TEMPLATE,
+  VALIDATION_INVALID_CLASSES,
+  VALIDATION_MAP_ERRORS_FN,
+  VALIDATION_TARGET_SELECTOR,
+  VALIDATION_VALIDATE_ON_SUBMIT,
+  ValidationErrorComponent,
+} from '@ngx-validate/core';
+import { DEFAULT_VALIDATION_BLUEPRINTS } from './validation';
+import { CustomValidationErrorComponent } from './validation-error.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,11 +32,46 @@ export const appConfig: ApplicationConfig = {
       withOptions({
         environment,
         registerLocaleFn: registerLocaleForEsBuild(),
-      }),
+      })
     ),
+    // aded for default missed auth service
     {
       provide: AuthService,
       useClass: CustomAuthService,
     },
-  ]
+    //"@ngx-validate/core": "^0.2.0", for validation
+    {
+      provide: VALIDATION_VALIDATE_ON_SUBMIT,
+      useValue: true,
+    },
+    {
+      provide: VALIDATION_MAP_ERRORS_FN,
+      useValue: defaultMapErrorsFn,
+    },
+    {
+      provide: VALIDATION_TARGET_SELECTOR, // componente objetivo selector de clase
+      useValue: '.field',
+    },
+    {
+      provide: VALIDATION_INVALID_CLASSES, // clase a agregar incencesario ya que el compontnet ya le agrega, pero se requiere el injector para q compile
+      useValue: 'field-error-class',
+    },
+    {
+      provide: VALIDATION_BLUEPRINTS,
+      // useValue: { ...BLUEPRINTS }, // default
+      useValue: { ...DEFAULT_VALIDATION_BLUEPRINTS }, // para usar con abp y junto con el template para que funcione bien
+    },
+    {
+      provide: VALIDATION_ERROR_TEMPLATE,
+      // useValue: ValidationErrorComponent, // default emplate
+      useValue: CustomValidationErrorComponent,
+    },
+  ],
 };
+// available tokens
+// export * from './blueprints.token'; // USED
+// export * from './error-template.token';// USED
+// export * from './invalid-classes.token'; // USED
+// export * from './map-errors-fn.token'; // USED
+// export * from './target-selector.token'; // USED
+// export * from './validate-on-submit.token'; // USED
--- Angular/frontapp/src/app/app.ts
+++ Angular/frontapp/src/app/app.ts
@@ -1,24 +1,57 @@
-import { ApplicationConfigurationDto, ConfigStateService } from '@abp/ng.core';
+import { AbpValidators, ApplicationConfigurationDto, ConfigStateService } from '@abp/ng.core';
 import { JsonPipe } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
+import { NgxValidateCoreModule } from '@ngx-validate/core';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JsonPipe],
+  // imports: [RouterOutlet, JsonPipe, ReactiveFormsModule, NgxValidateCoreModule.forRoot()],
+  imports: [RouterOutlet, JsonPipe, ReactiveFormsModule, NgxValidateCoreModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   protected readonly title = signal('frontapp');
   appConfig: ApplicationConfigurationDto = {} as ApplicationConfigurationDto;
+  formExample: FormGroup = new FormGroup({});
+  protected readonly configState = inject(ConfigStateService);
+  public formBuilder = inject(FormBuilder);
   ngOnInit(): void {
     this.appConfig = this.configState.getAll();
+
+    this.formExample = this.formBuilder.group<{
+      filter: FormControl<string | null>;
+    }>({
+      // NOTA: todos los validadores de abp se deben usar con la invocacacion de funciones ()
+      // can be use anything is indipendent why is configured abp DEFAULT_VALIDATION_BLUEPRINTS
+      // filter: new FormControl<string>('', [
+      //   AbpValidators.required(),
+      //   AbpValidators.stringLength({
+      //     maximumLength: 3,
+      //   }),
+      // ]),
+      filter: new FormControl<string>('', [
+        Validators.required,
+        Validators.maxLength(10),
+        // Validators.email,
+        AbpValidators.emailAddress(),
+      ]),
+    });
   }
 
-  protected readonly configState = inject(ConfigStateService);
-
-
+  save() {
+    // validate form before save
+    // if (!this.form.valid || this.modalBusy) return;
+    if (this.formExample.invalid) return;
+    console.log(this.formExample.value);
+  }
 }
 // TODO: add all comon components
 // pipes, directives, services, etc.
