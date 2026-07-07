41d1744542e525ef35bd4e7be26b8856a64da59b
Washington Acero <shin321654978@gmail.com>
2026-07-07 16:58:21 -0500

    delted duplicated files

--- Angular/frontapp/src/app/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './interceptors/api.interceptor';
-export * from './services/custom-auth.service';
-export * from './abp-utils/interfaces';
-export * from './abp-utils/abp-utils.providers';
