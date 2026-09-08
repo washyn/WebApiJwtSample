02d9410ff61079a81470a4b97e3f6401749ff3de
Washington Acero M <shin321654987@gmail.com>
2026-09-07 20:16:30 -0700

    aded example of hjangfire

---
+++ HangFireCron/Data/WebDbContextFactory.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Acme.BookStore.Web.Data;
+
+public class WebDbContextFactory : IDesignTimeDbContextFactory<WebDbContext>
+{
+    public WebDbContext CreateDbContext(string[] args)
+    {
+
+        var configuration = BuildConfiguration();
+
+        var builder = new DbContextOptionsBuilder<WebDbContext>()
+            .UseSqlite(configuration.GetConnectionString("Default"));
+
+        return new WebDbContext(builder.Options);
+    }
+
+    private static IConfigurationRoot BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false);
+
+        return builder.Build();
+    }
+}
